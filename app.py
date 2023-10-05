a3a8f3e33bcc0188006a2ba57dd56f9a532002e6
Blake Fullerton <43489000+blaketylerfullerton@users.noreply.github.com>
2023-10-05 14:46:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
 #import requests
-import mysql.connector
+#import mysql.connector
 import json
 
 app = Flask(__name__)
 
 # Initialize MySQL database connection
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Shane199!",
-    database="athenasql"
-)
-cursor = db.cursor()
+#db = mysql.connector.connect(
+ #   host="localhost",
+  #  user="root",
+   # password="Shane199!",
+    #database="athenasql"
+#)
+#cursor = db.cursor()
 
 @app.route('/')
 def index():
@@ -36,26 +36,26 @@ def thank_you():
 
 @app.route('/freeversion',methods=["GET", "POST"])
 def freeversion():
-    if request.method == "POST":
+    #if request.method == "POST":
         # Get user input from the form
-        name = request.form["name"]
-        canvas_url = request.form["canvas_url"]
-        api_token = request.form["api_token"]
-        phone_number = request.form["phone_number"]
+        #name = request.form["name"]
+        #canvas_url = request.form["canvas_url"]
+        #api_token = request.form["api_token"]
+        #phone_number = request.form["phone_number"]
         
         # Insert the user data into the MySQL database
-        query = "INSERT INTO users (name, canvas_url, api_token, phone_number) VALUES (%s, %s, %s, %s)"
-        values = (name, canvas_url, api_token, phone_number)
-        cursor.execute(query, values)
-        db.commit()
+        #query = "INSERT INTO users (name, canvas_url, api_token, phone_number) VALUES (%s, %s, %s, %s)"
+        #values = (name, canvas_url, api_token, phone_number)
+        #cursor.execute(query, values)
+        #db.commit()
 
         # Redirect to the "thank_you" route with student_data as a URL parameter
-        return redirect(url_for("thank_you", student_data=json.dumps({
-            'name': name,
-            'canvas_api_token': api_token,
-            'canvas_url': canvas_url,
-            'phone_number': phone_number
-        })))
+        #return redirect(url_for("thank_you", student_data=json.dumps({
+         #   'name': name,
+          #  'canvas_api_token': api_token,
+           # 'canvas_url': canvas_url,
+            #'phone_number': phone_number
+        3})))
 
 
     
